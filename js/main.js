98b9644f3f06ab06f4c119092279ae13f01048d7
nonsuke0113 <asinal.iip36@icloud.com>
2024-01-24 10:53:01 +0900

    create todo app

--- js/main.js
+++ js/main.js
@@ -1,6 +1,24 @@
 const app = Vue.createApp({
-    data: () => ({
-        message: 'Hello Vue.js!'
-    })
+  data: () => ({
+    newItem: '',
+    todos: []
+  }),
+  methods: {
+    addItem: function() {
+      console.log("Clicked!")
+      if(this.newItem === '') return
+      let todo = {
+        item: this.newItem,
+        isDone: false
+      }
+      this.todos.push(todo)
+      this.newItem = ''
+    },
+    deleteItem: function(index) {
+      console.log("Delete!")
+      console.log(index)
+      this.todos.splice(index, 1)
+    }
+  }
 })
 app.mount('#app')
