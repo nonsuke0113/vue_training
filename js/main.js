b9e052537e2d26fd2f0f2d89806024848ae9e2d3
nonsuke0113 <asinal.iip36@icloud.com>
2024-01-24 17:22:49 +0900

    training watch

--- js/main.js
+++ js/main.js
@@ -2,12 +2,53 @@ const app = Vue.createApp({
   data: () => ({
     basePrice: 100,
     message: 'Hello <span style="color:red">Vue.js!</span>',
+    message7: 'Hello Vue.js!',
+    km: 0,
+    m: 0,
+    firstName: '',
+    lastName: '',
+    fullName: '',
+    colors: [
+      { name: 'Red' },
+      { name: 'Green' },
+      { name: 'Blue' },
+    ],
     number: 100,
     ok: true,
     newItem: '',
     todos: []
   }),
+  watch: {
+    colors: {
+      handler: function(newValue, oldValue) {
+        console.log('Update!')
+      },
+      deep: true
+    },
+    message7: function(newValue, oldValue) {
+      console.log('new: %s, old: %s', newValue, oldValue)
+    },
+    km: function(value) {
+      console.log(value)
+      this.km = value
+      this.m = value * 1000
+    },
+    m: function(value) {
+      console.log(value)
+      this.km = value / 1000
+      this.m = value
+    },
+    firstName: function(value) {
+      this.fullName = value + ' ' + this.lastName
+    },
+    lastName: function(value) {
+      this.fullName = this.firstName + ' ' + value
+    }
+  },
   computed: {
+    fullNameComputed: function() {
+      return this.firstName + ' ' + this.lastName
+    },
     reversedMessage: function() {
       return this.message.split('').reverse().join('')
     },
@@ -24,6 +65,9 @@ const app = Vue.createApp({
     }
   },
   methods: {
+    onClick: function() {
+      this.colors[1].name = 'White'
+    },
     methodsNumber: function() {
       return Math.random()
     },
